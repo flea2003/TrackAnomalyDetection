8988c6e16a13ea58e6f712c08c72b8d599018d25
justinas <J.Jucas@student.tudelft.nl>
2024-05-16 16:39:41 +0200

    Passing pipeline

--- backend/src/main/java/sp/utils/UtilsObjectMapper.java
+++ backend/src/main/java/sp/utils/UtilsObjectMapper.java
@@ -5,6 +5,9 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class UtilsObjectMapper extends ObjectMapper {
+    /**
+     * Our manual Object mapper used in serialization to and from JSON.
+     */
     public UtilsObjectMapper() {
         super();
         this.registerModule(new JavaTimeModule());
