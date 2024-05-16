fcb9e24f4bf389c2b56f2f944e62728e56866032
justinas <J.Jucas@student.tudelft.nl>
2024-05-16 15:48:39 +0200

    Small fix of UtilsObjectClass

--- backend/src/main/java/sp/utils/UtilsObjectMapper.java
+++ backend/src/main/java/sp/utils/UtilsObjectMapper.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 
 public class UtilsObjectMapper extends ObjectMapper {
     public UtilsObjectMapper() {
+        super();
         this.registerModule(new JavaTimeModule());
         this.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
     }
