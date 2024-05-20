5455f3e05115db1838522ac95bc74273d7856204
Marius Frija <m.frija@student.tudelft.nl>
2024-05-20 14:42:06 +0200

    fixed backend tests after refactoring

--- backend/src/test/java/sp/controllers/ShipsDataControllerTest.java
+++ backend/src/test/java/sp/controllers/ShipsDataControllerTest.java
@@ -1,165 +1,110 @@
-//package sp.controllers;
-//
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.http.ResponseEntity;
-//import sp.model.AnomalyInformation;
-//import sp.exceptions.NotExistingShipException;
-//import sp.exceptions.PipelineException;
-//import sp.model.AISSignal;
-//import sp.model.CurrentShipDetails;
-//import sp.services.ShipsDataService;
-//
-//import java.time.OffsetDateTime;
-//import java.time.ZoneOffset;
-//import java.util.List;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.Mockito.*;
-//
-//class ShipsDataControllerTest {
-//
-//    private ShipsDataService shipsDataService;
-//    private ShipsDataController shipsDataController;
-//
-//    private final OffsetDateTime time1 = OffsetDateTime.of(2004, 1, 27, 1,1,0,0, ZoneOffset.ofHours(0));
-//
-//    @BeforeEach
-//    void setUp() {
-//        shipsDataService = mock(ShipsDataService.class);
-//        shipsDataController = new ShipsDataController(shipsDataService);
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        shipsDataController = null;
-//        shipsDataService = null;
-//    }
-//
-//    @Test
-//    void getCurrentAISInformationSuccessful() throws PipelineException, NotExistingShipException {
-//        long shipId = 123L;
-//        when(shipsDataService.getCurrentAISInformation(shipId)).thenReturn(
-//                new AISSignal(shipId, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, time1, "port1")
-//        );
-//
-//        ResponseEntity<AISSignal> response = shipsDataController.getCurrentAISInformation(shipId);
-//
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//        assertEquals(new AISSignal(
-//                shipId, 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, time1, "port1"
-//        ), response.getBody());
-//    }
-//
-//    @Test
-//    void getCurrentAISInformationShipDoesNotExist() throws PipelineException, NotExistingShipException {
-//        long shipId = 123L;
-//        when(shipsDataService.getCurrentAISInformation(shipId))
-//                .thenThrow(new NotExistingShipException());
-//
-//        ResponseEntity<AISSignal> response = shipsDataController.getCurrentAISInformation(shipId);
-//
-//        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-//    }
-//
-//    @Test
-//    void getCurrentAISInformationPipelineException() throws PipelineException, NotExistingShipException {
-//        long shipId = 123L;
-//        when(shipsDataService.getCurrentAISInformation(shipId))
-//                .thenThrow(new PipelineException());
-//
-//        ResponseEntity<AISSignal> response = shipsDataController.getCurrentAISInformation(shipId);
-//
-//        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-//    }
-//
-//    @Test
-//    void getCurrentAnomalyInformationSuccessful() throws PipelineException, NotExistingShipException {
-//        long shipId = 123L;
-//        when(shipsDataService.getCurrentAnomalyInformation(shipId)).thenReturn(
-//                new AnomalyInformation(1.0f, "explanation1", time1, shipId)
-//        );
-//
-//        ResponseEntity<AnomalyInformation> response = shipsDataController.getCurrentAnomalyInformation(shipId);
-//
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//        assertEquals(new AnomalyInformation(
-//                1.0f, "explanation1", time1, shipId
-//        ), response.getBody());
-//    }
-//
-//    @Test
-//    void getCurrentAnomalyInformationShipDoesNotExist() throws PipelineException, NotExistingShipException {
-//        long shipId = 123L;
-//        when(shipsDataService.getCurrentAnomalyInformation(shipId))
-//                .thenThrow(new NotExistingShipException());
-//
-//        ResponseEntity<AnomalyInformation> response = shipsDataController.getCurrentAnomalyInformation(shipId);
-//
-//        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
-//    }
-//
-//    @Test
-//    void getCurrentAnomalyInformationPipelineException() throws PipelineException, NotExistingShipException {
-//        long shipId = 123L;
-//        when(shipsDataService.getCurrentAnomalyInformation(shipId))
-//                .thenThrow(new PipelineException());
-//
-//        ResponseEntity<AnomalyInformation> response = shipsDataController.getCurrentAnomalyInformation(shipId);
-//
-//        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-//    }
-//
-//    @Test
-//    void getCurrentAISInformationOfAllShipsSuccessful() throws PipelineException {
-//        AISSignal signal1 = new AISSignal(1L, 1, 2, 3, 4, 5, time1, "port1");
-//        AISSignal signal2 = new AISSignal(2L, 6, 7, 8, 9, 10, time1, "port2");
-//        AISSignal signal3 = new AISSignal(3L, 11, 12, 13, 14, 15, time1, "port3");
-//
-//        when(shipsDataService.getCurrentAISInformationOfAllShips())
-//                .thenReturn(List.of(signal1, signal2, signal3));
-//
-//        ResponseEntity<List<AISSignal>> response = shipsDataController.getCurrentAISInformationOfAllShips();
-//
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//        assertEquals(List.of(signal1, signal2, signal3), response.getBody());
-//    }
-//
-//    @Test
-//    void getCurrentAISInformationOfAllShipsPipelineException() throws PipelineException {
-//        when(shipsDataService.getCurrentAISInformationOfAllShips())
-//                .thenThrow(new PipelineException());
-//
-//        ResponseEntity<List<AISSignal>> response = shipsDataController.getCurrentAISInformationOfAllShips();
-//
-//        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-//    }
-//
-//    @Test
-//    void getCurrentAnomalyInformationOfAllShipsSuccessful() throws PipelineException {
-//        AnomalyInformation info1 = new AnomalyInformation(1, "explanation1", time1, 1L);
-//        AnomalyInformation info2 = new AnomalyInformation(2, "explanation2", time1, 2L);
-//        AnomalyInformation info3 = new AnomalyInformation(3, "explanation3", time1, 3L);
-//
-//        when(shipsDataService.getCurrentAnomalyInformationOfAllShips())
-//                .thenReturn(List.of(info1, info2, info3));
-//
-//        ResponseEntity<List<AnomalyInformation>> response = shipsDataController.getCurrentAnomalyInformationOfAllShips();
-//
-//        assertEquals(HttpStatus.OK, response.getStatusCode());
-//        assertEquals(List.of(info1, info2, info3), response.getBody());
-//    }
-//
-//    @Test
-//    void getCurrentShipDetailsOfAllShipsPipelineException() throws PipelineException {
-//        when(shipsDataService.getCurrentShipDetails())
-//                .thenThrow(new PipelineException());
-//
-//        ResponseEntity<List<CurrentShipDetails>> response = shipsDataController.getCurrentShipDetails();
-//
-//        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
-//    }
-//
-//}
+package sp.controllers;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import sp.model.AnomalyInformation;
+import sp.exceptions.NotExistingShipException;
+import sp.exceptions.PipelineException;
+import sp.model.AISSignal;
+import sp.model.CurrentShipDetails;
+import sp.model.MaxAnomalyScoreDetails;
+import sp.services.ShipsDataService;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+class ShipsDataControllerTest {
+
+    private ShipsDataService shipsDataService;
+    private ShipsDataController shipsDataController;
+
+    private final OffsetDateTime time1 = OffsetDateTime.of(2004, 1, 27, 1,1,0,0, ZoneOffset.ofHours(0));
+
+    @BeforeEach
+    void setUp() {
+        shipsDataService = mock(ShipsDataService.class);
+        shipsDataController = new ShipsDataController(shipsDataService);
+    }
+
+    @AfterEach
+    void tearDown() {
+        shipsDataController = null;
+        shipsDataService = null;
+    }
+
+    @Test
+    void getCurrentShipDetailsSuccessful() throws PipelineException, NotExistingShipException {
+        long shipId = 123L;
+        when(shipsDataService.getIndividualCurrentShipDetails(shipId)).thenReturn(
+                new CurrentShipDetails(new AnomalyInformation(1.0f, "explanation1", time1, shipId),
+                        new AISSignal(shipId, 1F,1F,1F,1F,1F,time1,""),
+                        new MaxAnomalyScoreDetails(0F,time1))
+        );
+
+        ResponseEntity<CurrentShipDetails> response = shipsDataController.getIndividualCurrentShipDetails(shipId);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(new CurrentShipDetails(new AnomalyInformation(1.0f, "explanation1", time1, shipId),
+                new AISSignal(shipId, 1F,1F,1F,1F,1F,time1,""),
+                new MaxAnomalyScoreDetails(0F,time1)), response.getBody());
+    }
+
+    @Test
+    void getCurrentAnomalyInformationShipDoesNotExist() throws PipelineException, NotExistingShipException {
+        long shipId = 123L;
+        when(shipsDataService.getIndividualCurrentShipDetails(shipId))
+                .thenThrow(new NotExistingShipException());
+
+        ResponseEntity<CurrentShipDetails> response = shipsDataController.getIndividualCurrentShipDetails(shipId);
+
+        assertEquals(HttpStatus.NOT_FOUND, response.getStatusCode());
+    }
+
+    @Test
+    void getCurrentAnomalyInformationPipelineException() throws PipelineException, NotExistingShipException {
+        long shipId = 123L;
+        when(shipsDataService.getIndividualCurrentShipDetails(shipId))
+                .thenThrow(new PipelineException());
+
+        ResponseEntity<CurrentShipDetails> response = shipsDataController.getIndividualCurrentShipDetails(shipId);
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+
+
+    @Test
+    void getCurrentAnomalyInformationOfAllShipsSuccessful() throws PipelineException {
+        AnomalyInformation info1 = new AnomalyInformation(1, "explanation1", time1, 1L);
+        AnomalyInformation info2 = new AnomalyInformation(2, "explanation2", time1, 2L);
+        AnomalyInformation info3 = new AnomalyInformation(3, "explanation3", time1, 3L);
+        CurrentShipDetails details1 = new CurrentShipDetails(info1, null, null);
+        CurrentShipDetails details2 = new CurrentShipDetails(info2, null, null);
+        CurrentShipDetails details3 = new CurrentShipDetails(info3, null, null);
+        when(shipsDataService.getCurrentShipDetails())
+                .thenReturn(List.of(details1, details2, details3));
+
+        ResponseEntity<List<CurrentShipDetails>> response = shipsDataController.getCurrentShipDetails();
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+        assertEquals(List.of(details1, details2, details3), response.getBody());
+    }
+
+    @Test
+    void getCurrentShipDetailsOfAllShipsPipelineException() throws PipelineException {
+        when(shipsDataService.getCurrentShipDetails())
+                .thenThrow(new PipelineException());
+
+        ResponseEntity<List<CurrentShipDetails>> response = shipsDataController.getCurrentShipDetails();
+
+        assertEquals(HttpStatus.INTERNAL_SERVER_ERROR, response.getStatusCode());
+    }
+
+}
--- backend/src/test/java/sp/pipeline/AggregatorTest.java
+++ backend/src/test/java/sp/pipeline/AggregatorTest.java
@@ -1,163 +1,158 @@
-//package sp.pipeline;
-//
-//import com.fasterxml.jackson.core.JsonProcessingException;
-//import org.junit.jupiter.api.AfterEach;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import sp.model.*;
-//
-//import java.time.OffsetDateTime;
-//import java.time.ZoneOffset;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//
-//class AggregatorTest {
-//
-//    private Aggregator aggregator;
-//
-//    private final OffsetDateTime timestamp = OffsetDateTime.of(2015, 4, 18, 1,1,0,0, ZoneOffset.ofHours(0));
-//    private final OffsetDateTime timestamp2 = OffsetDateTime.of(2016, 4, 18, 1,1,0,0, ZoneOffset.ofHours(0));
-//
-//    @BeforeEach
-//    void setUp() {
-//        aggregator = new Aggregator();
-//    }
-//
-//    @AfterEach
-//    void tearDown() {
-//        aggregator = null;
-//    }
-//
-//    @Test
-//    void detailsNotInitializedFirstNull() {
-//        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
-//        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
-//        CurrentShipDetails details = new CurrentShipDetails(null, signal, maxInfo);
-//        assertTrue(aggregator.shipDetailsNotInitialized(details));
-//    }
-//
-//    @Test
-//    void detailsNotInitializedSecondNull() {
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
-//        CurrentShipDetails details = new CurrentShipDetails(info, null, maxInfo);
-//        assertTrue(aggregator.shipDetailsNotInitialized(details));
-//    }
-//
-//    @Test
-//    void detailsNotInitializedThirdNull() {
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//        AISSignal signal = new AISSignal(1,1F,1F,1F,1F,1F,timestamp,"");
-//        CurrentShipDetails details = new CurrentShipDetails(info, signal, null);
-//        assertTrue(aggregator.shipDetailsNotInitialized(details));
-//    }
-//
-//    @Test
-//    void detailsNotInitializedAllNonNull() {
-//        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
-//        CurrentShipDetails details = new CurrentShipDetails(info, signal, maxInfo);
-//        assertFalse(aggregator.shipDetailsNotInitialized(details));
-//    }
-//
-//    @Test
-//    void testEncapsulatesAnomalyWhenNull() {
-//        CurrentShipDetails details = new CurrentShipDetails(null, null, null);
-//
-//        assertFalse(aggregator.encapsulatesAnomalyInformation(null, details));
-//    }
-//
-//    @Test
-//    void testEncapsulatesAnomalyWhenNotFinalized() {
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//        CurrentShipDetails details = new CurrentShipDetails(info, null, null);
-//
-//        assertTrue(aggregator.encapsulatesAnomalyInformation(info, details));
-//    }
-//
-//    @Test
-//    void testEncapsulatesAnomalyBasedOnTimestampsFalse() {
-//        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
-//        CurrentShipDetails details = new CurrentShipDetails(info, signal, maxInfo);
-//
-//        assertFalse(aggregator.encapsulatesAnomalyInformation(info, details));
-//    }
-//
-//    @Test
-//    void testEncapsulatesAnomalyBasedOnTimestampsTrue() {
-//        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
-//        CurrentShipDetails details = new CurrentShipDetails(info, signal, maxInfo);
-//
-//        AnomalyInformation info2 = new AnomalyInformation(0.5f, "explain", timestamp2, 1L);
-//        assertTrue(aggregator.encapsulatesAnomalyInformation(info2, details));
-//    }
-//
-//    @Test
-//    void testEncapsulatesAISWhenNull() {
-//        CurrentShipDetails details = new CurrentShipDetails(null, null, null);
-//
-//        assertFalse(aggregator.encapsulatesAISSignal(null, details));
-//    }
-//
-//    @Test
-//    void testEncapsulatesAISWhenNotFinalized() {
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//        CurrentShipDetails details = new CurrentShipDetails(info, null);
-//
-//        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
-//        assertTrue(aggregator.encapsulatesAISSignal(signal, details));
-//    }
-//
-//    @Test
-//    void testEncapsulatesAISBasedOnTimestampsFalse() {
-//        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//        CurrentShipDetails details = new CurrentShipDetails(info, signal);
-//
-//        assertFalse(aggregator.encapsulatesAISSignal(signal, details));
-//    }
-//
-//    @Test
-//    void testEncapsulatesAISBasedOnTimestampsTrue() {
-//        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//        CurrentShipDetails details = new CurrentShipDetails(info, signal);
-//
-//        AISSignal signal2 = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp2, "port");
-//        assertTrue(aggregator.encapsulatesAISSignal(signal2, details));
-//    }
-//
-//    @Test
-//    void aggregateSignalsWithNewAIS() throws JsonProcessingException {
-//        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//
-//        CurrentShipDetails details = new CurrentShipDetails(info, null);
-//        ShipInformation shipInfo = new ShipInformation(1L, null, signal);
-//
-//        CurrentShipDetails result = aggregator.aggregateSignals(details, shipInfo.toJson());
-//
-//        assertSame(result, details);
-//        assertEquals(result.getCurrentAISSignal(), signal);
-//    }
-//
-//    @Test
-//    void aggregateSignalsWithNewAnomalyInfo() throws JsonProcessingException {
-//        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
-//        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
-//        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
-//        CurrentShipDetails details = new CurrentShipDetails(null, signal, maxInfo);
-//        ShipInformation shipInfo = new ShipInformation(1L, info, null);
-//
-//        CurrentShipDetails result = aggregator.aggregateSignals(details, shipInfo.toJson());
-//
-//        assertSame(result, details);
-//        assertEquals(result.getCurrentAnomalyInformation(), info);
-//    }
-//
-//}
+package sp.pipeline;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sp.model.*;
+
+import java.time.OffsetDateTime;
+import java.time.ZoneOffset;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AggregatorTest {
+
+    private Aggregator aggregator;
+
+    private final OffsetDateTime timestamp = OffsetDateTime.of(2015, 4, 18, 1,1,0,0, ZoneOffset.ofHours(0));
+    private final OffsetDateTime timestamp2 = OffsetDateTime.of(2016, 4, 18, 1,1,0,0, ZoneOffset.ofHours(0));
+
+    @BeforeEach
+    void setUp() {
+        aggregator = new Aggregator();
+    }
+
+    @AfterEach
+    void tearDown() {
+        aggregator = null;
+    }
+
+    @Test
+    void detailsNotInitializedFirstNull() {
+        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
+        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
+        CurrentShipDetails details = new CurrentShipDetails(null, signal, maxInfo);
+        assertTrue(aggregator.shipDetailsNotInitialized(details));
+    }
+
+    @Test
+    void detailsNotInitializedSecondNull() {
+        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
+        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
+        CurrentShipDetails details = new CurrentShipDetails(info, null, maxInfo);
+        assertTrue(aggregator.shipDetailsNotInitialized(details));
+    }
+
+    @Test
+    void detailsNotInitializedAllNonNull() {
+        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
+        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
+        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
+        CurrentShipDetails details = new CurrentShipDetails(info, signal, maxInfo);
+        assertFalse(aggregator.shipDetailsNotInitialized(details));
+    }
+
+    @Test
+    void testEncapsulatesAnomalyWhenNull() {
+        CurrentShipDetails details = new CurrentShipDetails(null, null, null);
+
+        assertFalse(aggregator.encapsulatesAnomalyInformation(null, details));
+    }
+
+    @Test
+    void testEncapsulatesAnomalyWhenNotFinalized() {
+        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
+        CurrentShipDetails details = new CurrentShipDetails(info, null, null);
+
+        assertTrue(aggregator.encapsulatesAnomalyInformation(info, details));
+    }
+
+    @Test
+    void testEncapsulatesAnomalyBasedOnTimestampsFalse() {
+        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
+        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
+        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
+        CurrentShipDetails details = new CurrentShipDetails(info, signal, maxInfo);
+
+        assertFalse(aggregator.encapsulatesAnomalyInformation(info, details));
+    }
+
+    @Test
+    void testEncapsulatesAnomalyBasedOnTimestampsTrue() {
+        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
+        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
+        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
+        CurrentShipDetails details = new CurrentShipDetails(info, signal, maxInfo);
+
+        AnomalyInformation info2 = new AnomalyInformation(0.5f, "explain", timestamp2, 1L);
+        assertTrue(aggregator.encapsulatesAnomalyInformation(info2, details));
+    }
+
+    @Test
+    void testEncapsulatesAISWhenNull() {
+        CurrentShipDetails details = new CurrentShipDetails(null, null, null);
+
+        assertFalse(aggregator.encapsulatesAISSignal(null, details));
+    }
+
+    @Test
+    void testEncapsulatesAISWhenNotFinalized() {
+        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
+        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
+        CurrentShipDetails details = new CurrentShipDetails(info, null, maxInfo);
+
+        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
+        assertTrue(aggregator.encapsulatesAISSignal(signal, details));
+    }
+
+    @Test
+    void testEncapsulatesAISBasedOnTimestampsFalse() {
+        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
+        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
+        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
+        CurrentShipDetails details = new CurrentShipDetails(info, signal, maxInfo);
+
+        assertFalse(aggregator.encapsulatesAISSignal(signal, details));
+    }
+
+    @Test
+    void testEncapsulatesAISBasedOnTimestampsTrue() {
+        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
+        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
+        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
+        CurrentShipDetails details = new CurrentShipDetails(info, signal, maxInfo);
+
+        AISSignal signal2 = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp2, "port");
+        assertTrue(aggregator.encapsulatesAISSignal(signal2, details));
+    }
+
+    @Test
+    void aggregateSignalsWithNewAIS() throws JsonProcessingException {
+        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
+        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
+        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
+        CurrentShipDetails details = new CurrentShipDetails(info, null, maxInfo);
+        ShipInformation shipInfo = new ShipInformation(1L, null, signal);
+
+        CurrentShipDetails result = aggregator.aggregateSignals(details, shipInfo.toJson());
+
+        assertSame(result, details);
+        assertEquals(result.getCurrentAISSignal(), signal);
+    }
+
+    @Test
+    void aggregateSignalsWithNewAnomalyInfo() throws JsonProcessingException {
+        AISSignal signal = new AISSignal(1L, 1f, 2f, 3f, 4f, 5f, timestamp, "port");
+        AnomalyInformation info = new AnomalyInformation(0.5f, "explain", timestamp, 1L);
+        MaxAnomalyScoreDetails maxInfo = new MaxAnomalyScoreDetails(1F, timestamp);
+        CurrentShipDetails details = new CurrentShipDetails(null, signal, maxInfo);
+        ShipInformation shipInfo = new ShipInformation(1L, info, null);
+
+        CurrentShipDetails result = aggregator.aggregateSignals(details, shipInfo.toJson());
+
+        assertSame(result, details);
+        assertEquals(result.getCurrentAnomalyInformation(), info);
+    }
+
+}
