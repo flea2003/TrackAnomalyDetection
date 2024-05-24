505a2ebf0b0719dc0d8abd1034b2a922f36a9e8a
Augustinas Jucas <augustinasjucas@gmail.com>
2024-05-24 18:33:47 +0200

    Added exception handling and logging to part of pipeline JSON things

--- backend/src/main/java/sp/pipeline/utils/json/KafkaJson.java
+++ backend/src/main/java/sp/pipeline/utils/json/KafkaJson.java
@@ -2,11 +2,18 @@ package sp.pipeline.utils.json;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import org.apache.kafka.streams.kstream.KStream;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class KafkaJson {
+    private static final Logger logger = LoggerFactory.getLogger(KafkaJson.class);
 
     /**
      * Deserialize a Kafka Stream of JSON strings to a KStream of objects of a given class type.
+     * In case of an exception during deserialization, we ignore the message and return an empty list.
      *
      * @param stream KStream of JSON strings
      * @param classType class type of the objects to deserialize to
@@ -14,11 +21,12 @@ public class KafkaJson {
      */
     public static <T> KStream<Long, T> deserialize(KStream<Long, String> stream,
                                                    Class<T> classType) {
-        return stream.mapValues(x -> {
+        return stream.flatMapValues(x -> {
             try {
-                return JsonMapper.fromJson(x, classType);
+                return List.of(JsonMapper.fromJson(x, classType));
             } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
+                logger.warn("Failed to deserialize JSON message, incoming to Kafka. Skipping this message. Message: " + x);
+                return new ArrayList<>();
             }
         });
     }
@@ -30,11 +38,12 @@ public class KafkaJson {
      * @return a KStream of JSON strings
      */
     public static <T> KStream<Long, String> serialize(KStream<Long, T> stream) {
-        return stream.mapValues(x -> {
+        return stream.flatMapValues(x -> {
             try {
-                return JsonMapper.toJson(x);
+                return List.of(JsonMapper.toJson(x));
             } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
+                logger.error("Failed to serialize object to JSON, outgoing from Kafka. Skipping this object. Object: " + x);
+                return new ArrayList<>();
             }
         });
     }
