42fcf853b9fa488a87e3f1fc2ee046c67759c9e6
Augustinas Jučas <A.Jucas@student.tudelft.nl>
2024-06-07 16:40:08 +0200

    Added custom json (de)serialization schemas

---
+++ backend/src/main/java/sp/pipeline/utils/json/MyJsonDeserializationSchema.java
@@ -0,0 +1,94 @@
+package sp.pipeline.utils.json;
+
+import org.apache.flink.api.common.typeinfo.TypeInformation;
+import org.apache.flink.formats.json.JsonDeserializationSchema;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.flink.util.function.SerializableSupplier;
+import org.apache.flink.util.jackson.JacksonMapperFactory;
+import java.io.IOException;
+
+/**
+ * This class is a very slightly modified version of the JsonDeserializationSchema class from the
+ * Flink library. It is used to deserialize JSON strings into Java objects. The only modification is
+ * that it ignores all exceptions that occur during deserialization and returns null instead of
+ * throwing an exception.
+ */
+public class MyJsonDeserializationSchema<T> extends JsonDeserializationSchema<T> {
+
+    private static final long serialVersionUID = 1L;
+
+    private final Class<T> clazz;
+    private final SerializableSupplier<ObjectMapper> mapperFactory;
+    protected transient ObjectMapper mapper;
+
+    /**
+     * Creates a JSON deserialization schema for the given class.
+     *
+     * @param clazz The class to which the JSON string is deserialized.
+     */
+    public MyJsonDeserializationSchema(Class<T> clazz) {
+        this(clazz, JacksonMapperFactory::createObjectMapper);
+    }
+
+    /**
+     * Creates a JSON deserialization schema for the given class.
+     *
+     * @param typeInformation Type information describing the result type. The type information is
+     *     used to create a Jackson {@link ObjectMapper} to parse the JSON.
+     */
+    public MyJsonDeserializationSchema(TypeInformation<T> typeInformation) {
+        this(typeInformation, JacksonMapperFactory::createObjectMapper);
+    }
+
+    /**
+     * Creates a JSON deserialization schema for the given class.
+     *
+     * @param clazz The class to which the JSON string is deserialized.
+     * @param mapperFactory A factory for creating ObjectMapper instances
+     */
+    public MyJsonDeserializationSchema(
+            Class<T> clazz, SerializableSupplier<ObjectMapper> mapperFactory) {
+        super(clazz);
+        this.clazz = clazz;
+        this.mapperFactory = mapperFactory;
+    }
+
+    /**
+     * Creates a JSON deserialization schema for the given class.
+     *
+     * @param typeInformation Type information describing the result type. The type information is
+     *     used to create a Jackson {@link ObjectMapper} to parse the JSON.
+     * @param mapperFactory A factory for creating {@link ObjectMapper} instances
+     */
+    public MyJsonDeserializationSchema(
+            TypeInformation<T> typeInformation, SerializableSupplier<ObjectMapper> mapperFactory) {
+        super(typeInformation);
+        this.clazz = typeInformation.getTypeClass();
+        this.mapperFactory = mapperFactory;
+    }
+
+    /**
+     * Creates a JSON deserialization schema for the given class.
+     *
+     * @param context the initialization context
+     */
+    @Override
+    public void open(InitializationContext context) {
+        mapper = mapperFactory.get();
+    }
+
+    /**
+     * Deserializes the JSON message.
+     *
+     * @param message The message, as a byte array.
+     * @return The deserialized message as an object.
+     */
+    @Override
+    public T deserialize(byte[] message) {
+        try {
+            return mapper.readValue(message, clazz);
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
