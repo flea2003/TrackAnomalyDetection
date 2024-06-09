be1e837a8fc2dfa4927772c9a235261e9ce930a1
Marius Frija <m.frija@student.tudelft.nl>
2024-06-09 16:53:07 +0200

    plot info

--- frontend/src/components/Side/MiddleColumn/ScorePlot/ScorePlot.tsx
+++ frontend/src/components/Side/MiddleColumn/ScorePlot/ScorePlot.tsx
@@ -2,10 +2,11 @@ import ShipDetails from "../../../../model/ShipDetails";
 import ShipNotification from "../../../../model/ShipNotification";
 import { CurrentPage } from "../../../../App";
 import Stack from "@mui/material/Stack";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HttpSender from "../../../../utils/communication/HttpSender";
 import plottingConfig from "../../../../configs/plottingConfig.json";
 import Plot from "react-plotly.js";
+import anomalyScorePlotStyle from "../../../../configs/anomalyScorePlotStyle.json";
 
 interface ScorePlotProps {
   ships: ShipDetails[];
@@ -18,28 +19,46 @@ function ScorePlot(props: ScorePlotProps){
 
   // Extract the props
   const allShips = props.ships;
+
   const allNotifications = props.notifications;
 
-  const selectedShip = allShips.find((x) => x.id === props.shipId);
+  const selectedShip = allShips.find((ship) => ship.id === props.shipId);
+
+  const [ shipHistory, setShipHistory ] = useState<ShipDetails[]>([]);
+
+  useEffect(() => {
+    const fetchShipHistory = async () => {
+      const fetchedShipHistory = await HttpSender.get(plottingConfig.historyEndpoint + props.shipId);
+      if (fetchedShipHistory) {
+        setShipHistory(fetchedShipHistory);
+      }
+    };
+    fetchShipHistory();
+  }, [props.shipId]);
 
   if (selectedShip === undefined) {
     return shipNotFoundElement();
   }
 
+  // Requires filtering out the default values assigned to incomplete details
+  const scoreHistory = shipHistory.map((ship) => ship.getAnomalyScore());
+  const timestampHistory = shipHistory.map((ship) => ship.timestamp);
+
   const shipNotifications = allNotifications.filter((notification) =>
     notification.shipDetails.id === props.shipId
   );
 
-  const notificationScores = allNotifications.map((notification) => notification.shipDetails.getAnomalyScore());
-  const notificationTimestamps = allNotifications.map((notification) => notification.shipDetails.timestamp);
+  const notificationScores = shipNotifications.map((notification) => notification.shipDetails.getAnomalyScore());
+  const notificationTimestamps = shipNotifications.map((notification) => notification.shipDetails.timestamp);
 
-  // Fetch the ShipDetails history of the selected ship
+  // Plot descriptions
+  const anomalyScoreDescriptions = scoreHistory.map((score, index) => {
+    return `Score: ${score}<br>Timestamp: ${timestampHistory[index]}`
+  });
 
-  const shipHistory: Promise<ShipDetails[]> = HttpSender.get(plottingConfig.historyEndpoint + props.shipId);
-
-  const scoreHistory = shipHistory.then((history) => history.map((ship) => ship.getAnomalyScore()));
-  const timestampHistory = shipHistory.then((history) => history.map((ship) => ship.timestamp));
-
+  const notificationDescriptions = notificationScores.map((score, index) => {
+    return `Score: ${score}<br>Timestamp: ${notificationTimestamps[index]}`
+  });
 
   return (
     <Plot
@@ -47,13 +66,25 @@ function ScorePlot(props: ScorePlotProps){
         {
           x: timestampHistory,
           y: scoreHistory,
-          type: 'line',
-          mode: 'lines+markers',
-          marker: { color: 'red' },
+          type: 'scatter',
+          mode: 'lines',
+          name: 'Anomaly Scores',
+          text: anomalyScoreDescriptions,
+          hoverinfo: 'text',
+          line: { color: 'blue'},
         },
-        { type: 'bar', x: notificationTimestamps, y: notificationScores },
+        {
+          x: notificationTimestamps,
+          y: notificationScores,
+          type: 'scatter',
+          mode: 'markers',
+          name: 'Notifications',
+          text: notificationDescriptions,
+          hoverinfo: 'text',
+          marker: { color: 'yellow', size: 8}
+        }
       ]}
-      layout={{ width: 720, height: 440, title: 'Anomaly Score Plot' }}
+      layout={anomalyScorePlotStyle}
     />
   );
 }
