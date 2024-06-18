fc913f8225ff4bc8ad34886abe049694c1fac791
Marius Frija <m.frija@student.tudelft.nl>
2024-06-18 17:44:31 +0200

    component improvements

--- frontend/src/components/Side/InformationContainer/ObjectDetails/ScorePlot.tsx
+++ frontend/src/components/Side/InformationContainer/ObjectDetails/ScorePlot.tsx
@@ -7,7 +7,7 @@ import ShipNotification from "../../../../model/ShipNotification";
 import React from "react";
 import PlotDataPointItem from "../../../../templates/PlotDataPointItem";
 import "../../../../styles/object-details/scorePlot.css";
-import { Stack } from "@mui/material";
+
 
 interface ScorePlotProps {
   ship: ShipDetails;
@@ -16,6 +16,7 @@ interface ScorePlotProps {
 }
 
 function ScorePlot(props: ScorePlotProps) {
+
   const selectedShipId = props.ship.id;
 
   const threshold = plottingConfig.notificationThreshold;
@@ -29,25 +30,21 @@ function ScorePlot(props: ScorePlotProps) {
   const notificationScoreHistory = shipNotifications.map(
     (notification) => notification.shipDetails.anomalyScore,
   );
-  console.log("NSCORE"+notificationScoreHistory);
+  console.log("NSCORE:{"+notificationScoreHistory+"}");
   const notificationTimestampHistory = shipNotifications.map(
-    (notification) => notification.shipDetails.timestamp,
+    (notification) => new Date(notification.shipDetails.timestamp).toLocaleDateString(),
   );
-  console.log("NTIME"+notificationTimestampHistory);
-  const shipHistory = props.displayedTrajectoryAndNotifications[0].map(
-    (trajectoryPoint) => {
-      return {
-        anomalyScore: trajectoryPoint.anomalyScore,
-        timestamp: trajectoryPoint.timestamp,
-      } as PlotDataPointItem;
-    },
-  );
+  console.log("NTIME:{"+notificationTimestampHistory+"}");
+
+  const shipHistory = preprocessHistory(props.displayedTrajectoryAndNotifications);
 
   const scoreHistory = shipHistory.map((dataPoint) => dataPoint.anomalyScore);
-  const timestampHistory = shipHistory.map((dataPoint) => dataPoint.timestamp);
-  console.log("SCORE"+scoreHistory);
-  console.log("TIME"+timestampHistory);
+  const timestampHistory = shipHistory.map((dataPoint) => new Date(dataPoint.timestamp).toLocaleDateString());
+
+  console.log("SCORE:{"+scoreHistory+"}");
+  console.log("TIME:{"+timestampHistory+"}");
   // Plot datapoint descriptions
+
   const anomalyScoreDescriptions = scoreHistory.map((score, index) => {
     return `Score: ${score}<br>Timestamp: ${timestampHistory[index]}`;
   });
@@ -59,18 +56,31 @@ function ScorePlot(props: ScorePlotProps) {
   );
 
   return (
-    <Stack className="plot-container">
+    <div className="plot-container">
       <Plot
         data={[
           {
             x: timestampHistory,
             y: scoreHistory,
             type: "scatter",
-            mode: "lines",
-            name: "Anomaly Scores",
+            mode: "lines+markers",
+            name: "Anomaly Score",
             text: anomalyScoreDescriptions,
             hoverinfo: "text",
+            hoverlabel: { // Customizing hover label for a specific trace
+              bgcolor: "#bbbbc3", // Background color
+              bordercolor: "#4c4949", // Border color
+              font: {
+                size: 8,
+                color: "#2e2b2b"
+              }
+            },
             line: { color: "blue" },
+            marker: {
+              color: "blue",
+              size: 5,
+              symbol: "circle"
+            }
           },
           {
             x: notificationTimestampHistory,
@@ -80,25 +90,60 @@ function ScorePlot(props: ScorePlotProps) {
             name: "Notifications",
             text: notificationDescriptions,
             hoverinfo: "text",
-            marker: { color: "yellow", size: 8 },
+            hoverlabel: { // Customizing hover label for a specific trace
+              bgcolor: "#e9f3b3", // Background color
+              bordercolor: "#4c4949", // Border color
+              font: {
+                size: 8,
+                color: "#2e2b2b"
+              }
+            },
+            marker: {
+              color: "yellow",
+              size: 5,
+              symbol: "square"
+            },
           },
         ]}
         layout={{
           ...anomalyScorePlotStyle,
+          autosize: true,
+          margin: {
+            r: 40,
+            l: 40,
+            t: 30,
+            b: 30
+          },
           xaxis: {
             title: "Timestamp",
+            titlefont: {
+              size: 10
+            },
             tickfont: { size: 6 },
-            showticklabels: false
+            showticklabels: true,
+            tickmode: 'array',
+            tickvals: [timestampHistory[0], timestampHistory[timestampHistory.length - 1]],
+            ticktext: [
+              timestampHistory[0],
+              timestampHistory[timestampHistory.length - 1]
+            ],
+            showgrid: false
           },
           yaxis: {
-            title: "Anomaly Score (%)",
             tickfont: { size: 6 },
+          },
+          legend: {
+            orientation: "h",
+            x: 0.5, // Center the legend horizontally
+            y: 1.1, // Position above the top of the plot area
+            xanchor: 'center', // Anchor the legend at its center
+            yanchor: 'bottom' // Anchor the legend just below the specified 'y' position
           },
           shapes: [
             {
               type: "line",
-              x0: timestampHistory[0],
-              x1: timestampHistory[timestampHistory.length - 1],
+              x0: scoreHistory[0],
+              x1: scoreHistory[scoreHistory.length-1],
               y0: threshold,
               y1: threshold,
               line: {
@@ -109,9 +154,44 @@ function ScorePlot(props: ScorePlotProps) {
             },
           ],
         }}
+        useResizeHandler={true}
+        config={{displayModeBar: false}}
       />
-    </Stack>
+    </div>
   );
 }
 
-export default ScorePlot;
+const preprocessHistory = (trajectoryData: TrajectoryPoint[][]) => {
+  const parsedFilteredData = trajectoryData[0].map(
+    (trajectoryPoint) => {
+      return {
+        anomalyScore: trajectoryPoint.anomalyScore,
+        timestamp: trajectoryPoint.timestamp,
+      } as PlotDataPointItem;
+    },
+  ).filter(trajectoryPnt => trajectoryPnt.anomalyScore !== -1);
+
+  // Remove duplicate instances
+  return Array.from(
+    new Set(parsedFilteredData.map(obj => JSON.stringify(obj)))
+  ).map(json => JSON.parse(json) as PlotDataPointItem);
+}
+
+function arePropsEqual(prevProps: ScorePlotProps, nextProps: ScorePlotProps) {
+  const sameShip = prevProps.ship.id === nextProps.ship.id;
+
+  const sameTrajectoryInfo = prevProps.displayedTrajectoryAndNotifications === nextProps.displayedTrajectoryAndNotifications ||
+    (prevProps.displayedTrajectoryAndNotifications.length === nextProps.displayedTrajectoryAndNotifications.length &&
+      prevProps.displayedTrajectoryAndNotifications.every((item, index) =>
+        item.length === nextProps.displayedTrajectoryAndNotifications[index].length &&
+        item.every((point, ptIndex) =>
+          point === nextProps.displayedTrajectoryAndNotifications[index][ptIndex])));
+
+  const sameNotifications = prevProps.notifications === nextProps.notifications ||
+    (prevProps.notifications.length === nextProps.notifications.length &&
+      prevProps.notifications.every((item, index) => item.id === nextProps.notifications[index].id));
+
+  return sameShip && sameTrajectoryInfo && sameNotifications;
+}
+
+export default React.memo(ScorePlot, arePropsEqual);
