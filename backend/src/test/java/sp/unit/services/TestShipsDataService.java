d788a8358c93b57daa6e5671dae84f4acb2d21d5
Justinas Jučas <j.jucas@student.tudelft.nl>
2024-06-18 13:41:27 +0200

    Added all the necessary tests controller and service classes

--- backend/src/test/java/sp/unit/services/TestShipsDataService.java
+++ backend/src/test/java/sp/unit/services/TestShipsDataService.java
@@ -4,23 +4,26 @@ import java.io.IOException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-import java.util.List;
+import java.util.*;
 import java.util.function.Predicate;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.MockedStatic;
 import org.mockito.Mockito;
+import sp.dtos.DatabaseExtractObject;
+import sp.dtos.TrajectoryObject;
 import sp.exceptions.DatabaseException;
 import sp.model.AnomalyInformation;
 import sp.exceptions.NotExistingShipException;
 import sp.model.AISSignal;
 import sp.model.CurrentShipDetails;
+import sp.model.Notification;
 import sp.pipeline.AnomalyDetectionPipeline;
 import java.time.OffsetDateTime;
 import java.time.ZoneOffset;
-import java.util.HashMap;
-import java.util.Map;
+
 import sp.pipeline.parts.aggregation.extractors.ShipInformationExtractor;
+import sp.services.NotificationService;
 import sp.services.ShipsDataService;
 import sp.utils.DruidConfig;
 import sp.utils.sql.FileReader;
@@ -35,12 +38,16 @@ import static org.mockito.Mockito.*;
 public class TestShipsDataService {
     private ShipsDataService shipsDataService;
     private AISSignal signal3;
+    private AISSignal signal7;
+    private AISSignal signal8;
+
     CurrentShipDetails currentShipDetails1;
     CurrentShipDetails currentShipDetails2;
     CurrentShipDetails currentShipDetails3;
     CurrentShipDetails currentShipDetails4;
     DruidConfig druidConfig;
     ShipInformationExtractor shipInformationExtractor;
+    NotificationService notificationService;
 
     @BeforeEach
     public void setUp() throws Exception {
@@ -53,6 +60,12 @@ public class TestShipsDataService {
         signal3 = new AISSignal(1L, 63.0f, 11.0f,
                 20.0f, 60.0f, 80.0f,
                 OffsetDateTime.of(2015, 04, 18, 1,1,0,0, ZoneOffset.ofHours(0)), "Amsterdam");
+        signal7 = new AISSignal(1L, 63.0f, 11.0f,
+                20.0f, 60.0f, 80.0f,
+                OffsetDateTime.of(2015, 04, 19, 1,1,0,0, ZoneOffset.ofHours(0)), "Amsterdam");
+        signal8 = new AISSignal(1L, 63.0f, 11.0f,
+                20.0f, 60.0f, 80.0f,
+                OffsetDateTime.of(2015, 04, 18, 12,1,0,0, ZoneOffset.ofHours(0)), "Amsterdam");
         AISSignal signal4 = new AISSignal(3L, 20.0f, 90.0f,
             20.0f, 60.0f, 80.0f,
             OffsetDateTime.of(2015, 04, 18, 1, 1, 0, 0, ZoneOffset.ofHours(0)), "Delft");
@@ -86,6 +99,8 @@ public class TestShipsDataService {
             put(4L, currentShipDetails4);
         }};
 
+        notificationService = mock(NotificationService.class);
+
         // Mock Druid where needed
         druidConfig = Mockito.mock(DruidConfig.class);
         Connection connection = Mockito.mock(Connection.class);
@@ -99,7 +114,7 @@ public class TestShipsDataService {
 
         AnomalyDetectionPipeline anomalyDetectionPipeline = mock(AnomalyDetectionPipeline.class);
         shipInformationExtractor = mock(ShipInformationExtractor.class);
-        shipsDataService = new ShipsDataService(anomalyDetectionPipeline, queryExecutor, shipInformationExtractor);
+        shipsDataService = new ShipsDataService(anomalyDetectionPipeline, queryExecutor, shipInformationExtractor, notificationService);
 
         doReturn(currentShipDetailsMap).when(shipInformationExtractor)
             .getFilteredShipDetails(any(Predicate.class));
@@ -174,7 +189,7 @@ public class TestShipsDataService {
         // Setup the broken
         AnomalyDetectionPipeline anomalyDetectionPipelineBroken = mock(AnomalyDetectionPipeline.class);
         QueryExecutor queryExecutorBroken = mock(QueryExecutor.class);
-        ShipsDataService shipsDataServiceBroken = new ShipsDataService(anomalyDetectionPipelineBroken, queryExecutorBroken, shipInformationExtractor);
+        ShipsDataService shipsDataServiceBroken = new ShipsDataService(anomalyDetectionPipelineBroken, queryExecutorBroken, shipInformationExtractor, notificationService);
         doThrow(DatabaseException.class).when(queryExecutorBroken)
                 .executeQueryOneLong(5, "src/main/resources/db/history.sql", CurrentShipDetails.class);
         assertThatThrownBy(() -> shipsDataServiceBroken.getHistoryOfShip(5L)).isInstanceOf(DatabaseException.class);
@@ -185,13 +200,86 @@ public class TestShipsDataService {
         // Setup the broken
         AnomalyDetectionPipeline anomalyDetectionPipelineBroken = mock(AnomalyDetectionPipeline.class);
         QueryExecutor queryExecutorBroken = mock(QueryExecutor.class);
-        ShipsDataService shipsDataServiceBroken = new ShipsDataService(anomalyDetectionPipelineBroken, queryExecutorBroken, shipInformationExtractor);
+        ShipsDataService shipsDataServiceBroken = new ShipsDataService(anomalyDetectionPipelineBroken, queryExecutorBroken, shipInformationExtractor, notificationService);
         doThrow(DatabaseException.class).when(queryExecutorBroken)
                 .executeQueryOneLong(5, "src/main/resources/db/history.sql", CurrentShipDetails.class);
         try(MockedStatic<FileReader> fileReader = mockStatic(FileReader.class)) {
             fileReader.when(() -> FileReader.readQueryFromFile(anyString())).thenThrow(IOException.class);
             assertThatThrownBy(() -> shipsDataServiceBroken.getHistoryOfShip(5L)).isInstanceOf(DatabaseException.class);
         }
+    }
+
+    @Test
+    void getSubsampledHistoryOfShipSQLNotFound() throws DatabaseException {
+        AnomalyDetectionPipeline anomalyDetectionPipelineBroken = mock(AnomalyDetectionPipeline.class);
+        QueryExecutor queryExecutorBroken = mock(QueryExecutor.class);
+        ShipsDataService shipsDataServiceBroken = new ShipsDataService(anomalyDetectionPipelineBroken, queryExecutorBroken, shipInformationExtractor, notificationService);
+        doThrow(DatabaseException.class).when(queryExecutorBroken)
+                .executeQueryOneLong(5, "src/main/resources/db/sampledHistory.sql", DatabaseExtractObject.class);
+        try (MockedStatic<FileReader> fileReader = mockStatic(FileReader.class)) {
+            fileReader.when(() -> FileReader.readQueryFromFile(anyString())).thenThrow(IOException.class);
+            assertThatThrownBy(() -> shipsDataServiceBroken.getSubsampledHistoryOfShip(5L)).isInstanceOf(DatabaseException.class);
+        }
+    }
+
+    @Test
+    void getSimpleTrajectoryNoNotifications() throws DatabaseException {
+        AnomalyDetectionPipeline anomalyDetectionPipeline = mock(AnomalyDetectionPipeline.class);
+        QueryExecutor queryExecutor = mock(QueryExecutor.class);
+        ShipsDataService shipsDataService = new ShipsDataService(anomalyDetectionPipeline, queryExecutor, shipInformationExtractor, notificationService);
+
+        DatabaseExtractObject object1 = new DatabaseExtractObject(signal3, 1F);
+        DatabaseExtractObject object2 = new DatabaseExtractObject(signal7, 2F);
+
+        List<DatabaseExtractObject> queryResult = new ArrayList<>(Arrays.asList(object1, object2));
+
+        when(queryExecutor.executeQueryOneLong(1, "src/main/resources/db/sampledHistory.sql", DatabaseExtractObject.class)).thenReturn(queryResult);
+        when(notificationService.getAllNotificationForShip(1L)).thenReturn(new ArrayList<>());
+
+        List<TrajectoryObject> finalResult = new ArrayList<>(Arrays.asList(
+                new TrajectoryObject(1L, 11F, 20F, signal7.getTimestamp(), 2F),
+                new TrajectoryObject(1L, 11F, 20F, signal3.getTimestamp(), 1F)
+        ));
+        assertThat(shipsDataService.getSubsampledHistoryOfShip(1L)).isEqualTo(finalResult);
+    }
+
+    @Test
+    void getSimpleTrajectoryWithNotificationsAboveThreshold() throws DatabaseException {
+        AnomalyDetectionPipeline anomalyDetectionPipeline = mock(AnomalyDetectionPipeline.class);
+        QueryExecutor queryExecutor = mock(QueryExecutor.class);
+        ShipsDataService shipsDataService = new ShipsDataService(anomalyDetectionPipeline, queryExecutor, shipInformationExtractor, notificationService);
+
+        List<DatabaseExtractObject> queryResult = new ArrayList<>();
+
+        queryResult.add(new DatabaseExtractObject(signal7, 2F));
+        DatabaseExtractObject object2 = new DatabaseExtractObject(signal7, 1F);
+        for (int i = 0; i < 2000; i++) {
+            DatabaseExtractObject object1 = new DatabaseExtractObject(signal3, 1F);
+            queryResult.add(object1);
+        }
+
+        when(queryExecutor.executeQueryOneLong(1, "src/main/resources/db/sampledHistory.sql", DatabaseExtractObject.class)).thenReturn(queryResult);
+
+        List<Notification> notificationServiceResult = new ArrayList<>(
+            Arrays.asList(
+                    new Notification(12L, 1L, false, new CurrentShipDetails(new AnomalyInformation(2F, "", null, 1L), signal7, null)),
+                    new Notification(12L, 1L, false, new CurrentShipDetails(new AnomalyInformation(4F, "", null, 1L), signal8, null)))
+        );
+
+        when(notificationService.getAllNotificationForShip(1L)).thenReturn(notificationServiceResult);
+        List<TrajectoryObject> finalResult = new ArrayList<>();
+
+        finalResult.add(new TrajectoryObject(1L, 11F, 20F, signal7.getTimestamp(), 2F));
+        finalResult.add(new TrajectoryObject(1L, 11F, 20F, signal7.getTimestamp(), 2F));
+        finalResult.add(new TrajectoryObject(1L, 11F, 20F, signal8.getTimestamp(), 4F));
+
+        for (int i = 0; i < 1000; i++) {
+            TrajectoryObject object1 = new TrajectoryObject(1L, 11F, 20F, signal3.getTimestamp(), 1F);
+            finalResult.add(object1);
+        }
+
+        assertThat(shipsDataService.getSubsampledHistoryOfShip(1L).size()).isEqualTo(1003);
+        assertThat(shipsDataService.getSubsampledHistoryOfShip(1L)).isEqualTo(finalResult);
     }
 }
 
