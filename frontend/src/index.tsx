26de34a38906bb1e22ae45d0ddbe3ae8f0b63432
Victor Purice <v.purice@student.tudelft.nl>
2024-06-18 20:13:32 +0200

    revert index.tsx

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom/client";
 import "./styles/index.css";
 
 import App from "./App";
+import { NotificationService } from "./services/NotificationService";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement,
 );
+
+NotificationService.initializeReadNotifications();
+
 root.render(
+  // <React.StrictMode>
   <App />,
+  // </React.StrictMode>
 );
